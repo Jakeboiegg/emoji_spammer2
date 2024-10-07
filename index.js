e65d963bae93340aa7d948d25449170b76633310
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-07 22:48:32 +0800

    press text to copied

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import {emoji_spam_generator} from "./emoji_generator.js";
 var emoji_list = ["🔥", "🥰", "🌈", "✨", "💅", "✅"];
 var emoji_spam = emoji_spam_generator(emoji_list,108)
 
+var title = document.getElementById("title")
 var display = document.getElementById("display")
 
 display.innerHTML = emoji_spam
 display.addEventListener("click",function(){
   navigator.clipboard.writeText(emoji_spam)
+  title.innerHTML = "press text to copied"
 })
+
