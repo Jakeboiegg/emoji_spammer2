98e7049f6bd42d9d59efa59442028260e883134b
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-07 22:52:41 +0800

    spamme!!!

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import {emoji_spam_generator} from "./emoji_generator.js";
+import { emoji_spam_generator } from "./emoji_generator.js";
 
+var amount_to_spam = 108
 var emoji_list = ["🔥", "🥰", "🌈", "✨", "💅", "✅"];
-var emoji_spam = emoji_spam_generator(emoji_list,108)
+var emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
 
-var title = document.getElementById("title")
-var display = document.getElementById("display")
+var title = document.getElementById("title");
+var display = document.getElementById("display");
 
-display.innerHTML = emoji_spam
-display.addEventListener("click",function(){
-  navigator.clipboard.writeText(emoji_spam)
-  title.innerHTML = "press text to copied"
-})
-
+display.innerHTML = emoji_spam;
+display.addEventListener("click", function() {
+  navigator.clipboard.writeText(emoji_spam);
+  title.innerHTML = "press text to copied";
+  emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
+  display.innerHTML = emoji_spam
+});
