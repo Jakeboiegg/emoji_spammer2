9233d24fb55f8ea2b47a66afabc575aaa488f6c3
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-07 18:45:05 +0800

    initial commit

---
+++ emoji_generator.js
@@ -0,0 +1,30 @@
+export function emoji_spam_generator(list, total_length) {
+  function randomint(lowest, highest) {
+    var solution = Math.random();
+    solution = solution * (highest - lowest + 1);
+    solution = Math.floor(solution);
+    solution += lowest;
+    return solution;
+  }
+
+  function choose_random_item(list) {
+    var item_index = randomint(0, list.length - 1);
+    var item = list[item_index];
+    return item;
+  }
+
+  var emoji_spam_text = "";
+  var character_count = 0;
+  while (character_count < total_length) {
+    var emoji = choose_random_item(list);
+    var spam_amount = randomint(2, 4);
+    for (let i = 0; i < spam_amount; i++) {
+      emoji_spam_text += emoji;
+      character_count += 1;
+      if (character_count == total_length) {
+        break;
+      }
+    }
+  }
+  return emoji_spam_text;
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+import {emoji_spam_generator} from "./emoji_generator.js";
+
+var emoji_list = ["🔥", "🥰", "🌈", "✨", "💅", "✅"];
+var emoji_spam = emoji_spam_generator(emoji_list,70)
+
+var display = document.getElementById("display")
+
+display.innerHTML = emoji_spam
+display.addEventListener("click",function(){
+  navigator.clipboard.writeText(emoji_spam)
+})
