f95844833c05840df60cac6bca2cb1a9be609141
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-08 14:03:12 +0800

    pls pls thud

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ var emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
 var title = document.getElementById("title");
 var display = document.getElementById("display");
 
-display.innerHTML = emoji_spam;
-display.addEventListener("click", function() {
+function button_pressed() {
   navigator.clipboard.writeText(emoji_spam);
   title.innerHTML = "press text to copied";
   if (navigator.vibrate) {
@@ -16,7 +15,11 @@ display.addEventListener("click", function() {
   }
   emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
   display.innerHTML = emoji_spam;
-});
+}
+
+display.innerHTML = emoji_spam;
+display.addEventListener("mousedown",button_pressed);
+display.addEventListener("touchend",button_pressed);
 
 // Register the service worker
 if ("serviceWorker" in navigator) {
