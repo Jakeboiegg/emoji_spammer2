c215507f40cb8e82723416a0283d59fff9e9e792
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-08 14:09:04 +0800

    reverting all vibration shizz bc ios doesnt support
    This reverts commit f95844833c05840df60cac6bca2cb1a9be609141.
    
    Revert "ple thub"
    
    This reverts commit b984c03e4ab6cbe37a027b86b3ce76ad663678c0.
    
    Revert "thud"
    
    This reverts commit 9465e25057ae98975362ba8500ab9c9ea92bcca5.

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 import { emoji_spam_generator } from "./emoji_generator.js";
 
-var amount_to_spam = 12 * 20;
+var amount_to_spam = 12 * 20
 var emoji_list = ["🔥", "🥰", "🌈", "✨", "💅", "✅"];
 var emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
 
 var title = document.getElementById("title");
 var display = document.getElementById("display");
 
-function button_pressed() {
+display.innerHTML = emoji_spam;
+display.addEventListener("click", function() {
   navigator.clipboard.writeText(emoji_spam);
   title.innerHTML = "press text to copied";
-  if (navigator.vibrate) {
-    navigator.vibrate([50, 10, 50]);
-  }
   emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
-  display.innerHTML = emoji_spam;
-}
+  display.innerHTML = emoji_spam
+});
 
-display.innerHTML = emoji_spam;
-display.addEventListener("mousedown",button_pressed);
-display.addEventListener("touchend",button_pressed);
 
 // Register the service worker
-if ("serviceWorker" in navigator) {
+if ('serviceWorker' in navigator) {
   // Wait for the 'load' event to not block other work
-  window.addEventListener("load", async () => {
+  window.addEventListener('load', async () => {
     // Try to register the service worker.
     try {
       // Capture the registration for later use, if needed
@@ -32,15 +27,16 @@ if ("serviceWorker" in navigator) {
 
       // Use ES Module version of our Service Worker in development
       if (import.meta.env?.DEV) {
-        reg = await navigator.serviceWorker.register("/service-worker.js", {
-          type: "module",
+        reg = await navigator.serviceWorker.register('/service-worker.js', {
+          type: 'module',
         });
       } else {
         // In production, use the normal service worker registration
-        reg = await navigator.serviceWorker.register("/service-worker.js");
+        reg = await navigator.serviceWorker.register('/service-worker.js');
       }
+
     } catch (err) {
-      console.log("?? Service worker registration failed: ", err);
+      console.log('?? Service worker registration failed: ', err);
     }
   });
 }
