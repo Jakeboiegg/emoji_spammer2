6bea5f5df2634d9ede4b754e71aaf7a2fae0e99f
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-08 13:16:03 +0800

    service worker

--- index.js
+++ index.js
@@ -14,3 +14,29 @@ display.addEventListener("click", function() {
   emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
   display.innerHTML = emoji_spam
 });
+
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  // Wait for the 'load' event to not block other work
+  window.addEventListener('load', async () => {
+    // Try to register the service worker.
+    try {
+      // Capture the registration for later use, if needed
+      let reg;
+
+      // Use ES Module version of our Service Worker in development
+      if (import.meta.env?.DEV) {
+        reg = await navigator.serviceWorker.register('/service-worker.js', {
+          type: 'module',
+        });
+      } else {
+        // In production, use the normal service worker registration
+        reg = await navigator.serviceWorker.register('/service-worker.js');
+      }
+
+    } catch (err) {
+      console.log('?? Service worker registration failed: ', err);
+    }
+  });
+}
