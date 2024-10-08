f6d7377ba04738a3ea35ee98280d5c433f304cf5
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-08 11:28:28 +0800

    joiner

--- emoji_generator.js
+++ emoji_generator.js
@@ -20,6 +20,8 @@ export function emoji_spam_generator(list, total_length) {
     var spam_amount = randomint(2, 4);
     for (let i = 0; i < spam_amount; i++) {
       emoji_spam_text += emoji;
+      // add a "zero width space" for formatting reasons
+      emoji_spam_text += "\u200B";
       character_count += 1;
       if (character_count == total_length) {
         break;
