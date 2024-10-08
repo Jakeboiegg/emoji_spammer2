b984c03e4ab6cbe37a027b86b3ce76ad663678c0
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-10-08 13:45:51 +0800

    ple thub

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { emoji_spam_generator } from "./emoji_generator.js";
 
-var amount_to_spam = 12 * 20
+var amount_to_spam = 12 * 20;
 var emoji_list = ["🔥", "🥰", "🌈", "✨", "💅", "✅"];
 var emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
 
@@ -11,16 +11,17 @@ display.innerHTML = emoji_spam;
 display.addEventListener("click", function() {
   navigator.clipboard.writeText(emoji_spam);
   title.innerHTML = "press text to copied";
-  navigator.vibrate([50,10,50])
+  if (navigator.vibrate) {
+    navigator.vibrate([50, 10, 50]);
+  }
   emoji_spam = emoji_spam_generator(emoji_list, amount_to_spam);
-  display.innerHTML = emoji_spam
+  display.innerHTML = emoji_spam;
 });
 
-
 // Register the service worker
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   // Wait for the 'load' event to not block other work
-  window.addEventListener('load', async () => {
+  window.addEventListener("load", async () => {
     // Try to register the service worker.
     try {
       // Capture the registration for later use, if needed
@@ -28,16 +29,15 @@ if ('serviceWorker' in navigator) {
 
       // Use ES Module version of our Service Worker in development
       if (import.meta.env?.DEV) {
-        reg = await navigator.serviceWorker.register('/service-worker.js', {
-          type: 'module',
+        reg = await navigator.serviceWorker.register("/service-worker.js", {
+          type: "module",
         });
       } else {
         // In production, use the normal service worker registration
-        reg = await navigator.serviceWorker.register('/service-worker.js');
+        reg = await navigator.serviceWorker.register("/service-worker.js");
       }
-
     } catch (err) {
-      console.log('?? Service worker registration failed: ', err);
+      console.log("?? Service worker registration failed: ", err);
     }
   });
 }
